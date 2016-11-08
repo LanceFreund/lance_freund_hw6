bbfe27481c91da260d886cc53fd0036fbeb85952
Lance Freund <lancefreund@mail.weber.edu>
2016-11-08 12:48:35 -0700

    script creation

---
+++ lance_freund_hw6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/python3
+
+# This is template intended for python modules
+# Coded By: Lance Freund
+# Weber State University
+# lancefreund@mail.weber.edu
+
+#---- Write Python source code below this text. Don't forget your  imports ---
+
+
+
+
+exit(0)
